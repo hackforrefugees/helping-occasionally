afd9b1b61b768f2fffd900b17833dabb0c14f520
csvan <christopher.svanefalk@gmail.com>
2015-12-05 15:27:51 +0100

    [API] Run rest configuration at startup.

--- Hack4karma-meteor/server/rest/rest.js
+++ Hack4karma-meteor/server/rest/rest.js
@@ -2,32 +2,45 @@
  * Created by csvan on 05/12/15.
  */
 
-var Rest = new Restivus({
-    apiPath: 'api/',
-    auth: {
-        token: 'auth.apiKey',
-        user: function () {
+Meteor.startup(() => {
+    let Api = new Restivus({
+        apiPath: 'api/',
+        auth: {
+            token: 'auth.apiKey',
+            user: function () {
+                return {
+                    userId: this.request.headers['user-id'],
+                    token: this.request.headers['login-token']
+                };
+            }
+        },
+        defaultHeaders: {
+            'Content-Type': 'application/json'
+        },
+        onLoggedIn: function () {
+            console.log(this.user.username + ' (' + this.userId + ') logged in');
+        },
+        onLoggedOut: function () {
+            console.log(this.user.username + ' (' + this.userId + ') logged out');
+        },
+        prettyJson: true,
+        useDefaultAuth: true,
+        version: 'v1'
+    });
+
+    // Add core models
+    Api.addCollection(Skills);
+    Api.addCollection(Causes);
+    Api.addCollection(Projects);
+
+    Api.addRoute('custom', {
+        get: function () {
             return {
-                userId: this.request.headers['user-id'],
-                token: this.request.headers['login-token']
+                status: 'success',
+                data: 'get something different'
             };
         }
-    },
-    defaultHeaders: {
-        'Content-Type': 'application/json'
-    },
-    onLoggedIn: function () {
-        console.log(this.user.username + ' (' + this.userId + ') logged in');
-    },
-    onLoggedOut: function () {
-        console.log(this.user.username + ' (' + this.userId + ') logged out');
-    },
-    prettyJson: true,
-    useDefaultAuth: true,
-    version: 'v1'
+    });
 });
 
-// Add core models
-Rest.addCollection(Skills);
-Rest.addCollection(Causes);
-Rest.addCollection(Projects);
+
