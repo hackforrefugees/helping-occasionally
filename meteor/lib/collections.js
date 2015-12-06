6607323c8949811746dddc2658bb2d0ed528a3b2
csvan <christopher.svanefalk@gmail.com>
2015-12-06 01:08:33 +0100

    [Arch] Re-add missing files after rename.

---
+++ meteor/lib/collections.js
@@ -0,0 +1,52 @@
+/**
+ * Created by csvan on 05/12/15.
+ */
+
+/**
+ * Core User model, aliased for convenience.
+ *
+ * @type {any}
+ */
+Users = Meteor.users;
+
+/**
+ * Projects, which represent tasks needed by Organisations which need to be done by Volunteers.
+ *
+ * @type {Mongo.Collection}
+ */
+Projects = new Mongo.Collection('projects');
+
+/**
+ * M2M mapping between Projects and their User members.
+ *
+ * @type {Mongo.Collection}
+ */
+ProjectMembers = new Mongo.Collection('projectMembers');
+
+/**
+ * Skills which a Volunteer may possess.
+ *
+ * @type {Mongo.Collection}
+ */
+Skills = new Mongo.Collection('skills');
+
+/**
+ * M2M mapping between Volunteers and their Skills.
+ *
+ * @type {Mongo.Collection}
+ */
+VolunteerSkills = new Mongo.Collection('volunteerSkills');
+
+/**
+ * Causes, which a Project is focused at (education, healthcare etc).
+ *
+ * @type {Mongo.Collection}
+ */
+Causes = new Mongo.Collection('causes');
+
+/**
+ * M2M mapping between Projects and Causes.
+ *
+ * @type {Mongo.Collection}
+ */
+ProjectCauses = new Mongo.Collection('projectCauses');
---
+++ meteor/server/rest/rest.js
@@ -0,0 +1,43 @@
+/**
+ * Created by csvan on 05/12/15.
+ */
+
+Meteor.startup(() => {
+
+    var Rest = new Restivus({
+        apiPath: 'api/',
+        auth: {
+            token: 'auth.apiKey',
+            user: function () {
+                return {
+                    userId: this.request.headers['user-id'],
+                    token: this.request.headers['login-token']
+                };
+            }
+        },
+
+        defaultHeaders: {
+            'Content-Type': 'application/json'
+        },
+
+        onLoggedIn: function () {
+            console.log(this.user.username + ' (' + this.userId + ') logged in');
+        },
+
+        onLoggedOut: function () {
+            console.log(this.user.username + ' (' + this.userId + ') logged out');
+        },
+        prettyJson: true,
+        useDefaultAuth: true
+    });
+
+    // Add core models
+    Rest.addCollection(Skills);
+    Rest.addCollection(Causes);
+    Rest.addCollection(Projects);
+
+    // Add native User model
+    Rest.addCollection(Meteor.users);
+});
+
+
