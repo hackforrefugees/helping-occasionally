8b36dd2bf772fced5e5b420a001a83469f550339
Marcus Grennborg <mgrennborg@hotmail.com>
2015-12-06 10:29:31 +0100

    App now uses live data from the rest service

--- androidapp/app/src/main/java/help4karma/models/Project.java
+++ androidapp/app/src/main/java/help4karma/models/Project.java
@@ -1,5 +1,6 @@
 package help4karma.models;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -12,18 +13,27 @@ import java.util.List;
  */
 public class Project {
 
-    public Project(String name) {
+    public Project(String id, String name) {
         this.name = name;
+        this.id = id;
     }
 
-    public Project (String name, double lat, double lon) {
+    public Project (String id, String name, double lat, double lon) {
+        this.id = id;
         this.name = name;
         this.lat = lat;
         this.lon = lon;
     }
 
+    private final String id;
     private String name;
     private String description;
+
+    private String date; // Todo: should be a proper date
+    private String location;
+
+    private int maxNumberOfCandidates;
+    private int currentNumberOfCandidates;
 
     private Double lat;
     private Double lon;
@@ -47,13 +57,61 @@ public class Project {
         return lon;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getMaxNumberOfCandidates() {
+        return maxNumberOfCandidates;
+    }
+
+    public void setMaxNumberOfCandidates(int maxNumberOfCandidates) {
+        this.maxNumberOfCandidates = maxNumberOfCandidates;
+    }
+
+    public int getCurrentNumberOfCandidates() {
+        return currentNumberOfCandidates;
+    }
+
+    public void setCurrentNumberOfCandidates(int currentNumberOfCandidates) {
+        this.currentNumberOfCandidates = currentNumberOfCandidates;
+    }
+
     public String getDescription() {
         return description;
     }
 
     public static List<Project> createProjectsFromJson(JSONObject object) throws JSONException {
         List<Project> projectList = new ArrayList<>();
-        projectList.add(new Project(object.getString("name"))); // Todo
+        JSONArray jsonProjects = object.getJSONArray("data");
+
+        for (int i = 0; i<jsonProjects.length(); i++) {
+            JSONObject jsonProject = jsonProjects.getJSONObject(i);
+            Project project = new Project(jsonProject.getString("_id"), jsonProject.getString("name"));
+            project.setDate(jsonProject.getString("date"));
+            project.setLocation(jsonProject.getString("location"));
+            project.setDescription(jsonProject.getString("description"));
+            project.setCurrentNumberOfCandidates(jsonProject.getInt("numberCandidate"));
+            project.setMaxNumberOfCandidates(jsonProject.getInt("maxNumberCandidate"));
+            projectList.add(project);
+        }
+
         return projectList;
     }
 
